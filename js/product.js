588f0292473cb5412e1c433d44ded300068a600d
Rustdj <81041717+Rustdj@users.noreply.github.com>
2021-11-04 00:08:54 +0300

    add post email

--- js/product.js
+++ js/product.js
@@ -68,17 +68,18 @@ const cartProductList = document.querySelector('.cart-content__list');
 const cart = document.querySelector('.cart');
 const cartQuantity = document.querySelector('.cart__quantity');
 const fullPrice = document.querySelector('.fullprice');
-let price = 0;
+let price = 0; // для выводв количества товаров в корзине
+let productArray = [];
 
-const randomId = () => {
+const randomId = () => { // связывает кнопки
     return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 };
 
-const priceWithoutSpaces = (str) => {
+const priceWithoutSpaces = (str) => {  // удаляет пробелы у строки с ценой
     return str.replace(/\s/g, '');
 };
 
-const normalPrice = (str) => {
+const normalPrice = (str) => {  // преобразует строку с суммой с пробелом '23 234 ₽'
     return String(str).replace(/(\d)(?=(\d\d\d)+([ˆ\d]|$))/g, '$1 ');
 };
 
@@ -109,13 +110,27 @@ const generateCartProduct = (img, title, price, id) => {
         <article class="cart-content__product cart-product" data-id='${id}'>
             <img src="${img}" alt="Макбук" class="cart-product__img">
                 <div class="cart-product__text">
-                <h3 class="cart-product__title">${normalPrice(price)}</h3>
-            <span class="cart-product__price">${price} ₽</span>
+                <h3 class="cart-product__title">${title}</h3>
+            <span class="cart-product__price">${normalPrice(price)} ₽</span>
             </div>
             <button class="cart-product__delete" aria-label="Удалить товар"></button>
             </article>
         </li>
     `;
+};
+
+// delete
+
+const deleteProduct = (productParent) => {
+
+    let id = productParent.querySelector('.cart-product').dataset.id;
+    document.querySelector(`.product[data-id='${id}']`).querySelector('.product__btn').disabled = false
+
+    let currentPrice = parseInt(priceWithoutSpaces(productParent.querySelector('.cart-product__price').textContent))
+    minusFullPrice(currentPrice);
+    printFullPrice();
+    productParent.remove();
+    printQuantity();
 };
 
 productsBtn.forEach(el => {
@@ -136,3 +151,121 @@ productsBtn.forEach(el => {
         self.dasabled = true; // disabled btn
     });
 });
+
+
+cartProductList.addEventListener('click', (e) => {
+    if (e.target.classList.contains('cart-product__delete')) {
+        deleteProduct(e.target.closest('.cart-content__item'))
+    }
+});
+
+// Modal
+
+const orderModalOpenProd = document.querySelector('.order-modal__btn');
+const orderModalList = document.querySelector('.order-modal__list');
+
+
+let flag = 0;
+
+orderModalOpenProd.addEventListener('click', (e) => {
+    if (flag == 0) {
+        orderModalOpenProd.classList.add('open');
+        orderModalList.style.display = 'block';
+        flag = 1;
+    } else {
+        orderModalOpenProd.classList.remove('open');
+        orderModalList.style.display = 'none';
+        flag = 0;
+    }
+});
+
+
+// Заполнение товарами и отправление на почту
+
+
+const generateModalProduct = (img, title, price, id) => {
+	return `
+		<li class="order-modal__item">
+			<article class="order-modal__product order-product" data-id="${id}">
+				<img src="${img}" alt="" class="order-product__img">
+				<div class="order-product__text">
+					<h3 class="order-product__title">${title}</h3>
+					<span class="order-product__price">${normalPrice(price)}</span>
+				</div>
+				<button class="order-product__delete">Удалить</button>
+			</article>
+		</li>
+	`;
+};
+
+const modal = new GraphModal({
+	isOpen: (modal) => {
+		console.log('opened');
+		let array = cartProductList.querySelector('.simplebar-content').children;
+		let fullprice = fullPrice.textContent;
+		let length = array.length;
+
+		document.querySelector('.order-modal__quantity span').textContent = `${length} шт`;
+		document.querySelector('.order-modal__summ span').textContent = `${fullprice}`;
+		for (item of array) {
+			console.log(item)
+			let img = item.querySelector('.cart-product__img').getAttribute('src');
+			let title = item.querySelector('.cart-product__title').textContent;
+			let priceString = priceWithoutSpaces(item.querySelector('.cart-product__price').textContent);
+			let id = item.querySelector('.cart-product').dataset.id;
+
+			orderModalList.insertAdjacentHTML('afterbegin', generateModalProduct(img, title, priceString, id));
+
+            let obj = {};
+            obj.title = title;
+            obj.price = priceString;
+            productArray.push(obj);
+
+        }
+
+       console.log(productArray)
+    },
+    isClose: () => {
+        console.log('closed');
+    }
+});
+
+document.querySelector('.order').addEventListener('submit', (e) => {
+    e.preventDefault();
+    let self = e.currentTarget;
+
+    let formData = new FormData(self);
+    let name = self.querySelector('[name="Имя"]').value;
+    let tel = self.querySelector('[name="Телефон"]').value;
+    let mail = self.querySelector('[name="Email"]').value;
+    formData.append('Товары', JSON.stringify(productArray));
+    formData.append('Имя', name);
+    formData.append('Телефон', tel);
+    formData.append('Email', mail);
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                console.log('Отправлено');
+            }
+        }
+    }
+
+    xhr.open('POST', 'mail.php', true);
+    xhr.send(formData);
+
+    self.reset();
+});
+
+
+let selector = document.querySelectorAll('input[type="tel"]');
+
+let im = new Inputmask('+7 (999) 999-99-99');
+
+im.mask(selector);
+
+
+
+
